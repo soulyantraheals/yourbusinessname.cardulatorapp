bdbfd722c84956e4bdd8ac621d9cf582a504cbb6
soulyantraheals <soulyantraheals@gmail.com>
2026-03-06 16:57:21 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,32 +1,17 @@
-const CACHE_NAME = 'cardulator-v1.2'; 
+const CACHE_NAME = 'mahindra-v1.2';
 const ASSETS = [
   './',
-  './index4.html', // Matches your current filename
+  './index.html', 
   './manifest.json',
-  './app-ads.txt',
-  './icons/range_of_cars.png' // Verified existing asset
+  './icons/cardulatorlogoinstally.png',
+  './icons/range_of_cars.png'
 ];
 
-// Install & Cache
 self.addEventListener('install', (e) => {
-  self.skipWaiting(); 
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
-// Activate & Cleanup old caches
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)));
-    })
-  );
-});
-
-// Network-first, fallback to cache
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  );
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
